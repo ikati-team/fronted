a5e9dbb8a39dbae080066ff5687e40476968d277
turleo <turleo@outlook.com>
2022-12-23 09:52:03 +1000

    📝 team editor added

---
+++ src/pages/teams/TeamsEditor.vue
@@ -0,0 +1,63 @@
+<template>
+  <main>
+    <div class="img">
+      <img class="avatar" alt="team avatar" src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg">
+      <input type="file" class="form-control">
+    </div>
+    <div class="main">
+      <input :value="name" placeholder="Название команды" class="header-input" id="name">
+      <input :value="slogan" placeholder="Слоган" id="slogan">
+      <ul class="list-group" v-for="member in members">
+        <li class="list-group-item">
+          <p>
+            <input :value="member.jobTitle" placeholder="Должность" id="job">
+            <router-link :to="{ name: 'UsersViewer', params: { id: member.id } }">@{{ member.nick }}</router-link>
+          </p>
+          <p>📧 <a :href="`mailto:${member.mail}`">{{ member.mail }}</a></p>
+        </li>
+        <li class="list-group-item">
+          + Ещё участник
+        </li>
+      </ul>
+      <button style="margin-top: 16px" class="btn btn-dark">Сохранить</button>
+    </div>
+  </main>
+</template>
+
+<script>
+import '@/assets/viewers.css'
+import '@/assets/editors.css'
+
+export default {
+  name: "TeamsViewer",
+  data () {
+    return {
+      name: 'Ikati',
+      slogan: '🚀 Мы делаем жизнь людей проще',
+      members: [
+        {
+          id: 1,
+          nick: 'leo2r',
+          jobTitle: 'Котик 🐈',
+          mail: 'leo2r@ikati.space'
+        },
+        {
+          id: 2,
+          nick: 'beavernotacat',
+          jobTitle: 'Хозяйн 🧑',
+          mail: 'beavernotacat@ikati.space'
+        },
+        {
+          id: 3,
+          nick: 'Bazzzber',
+          jobTitle: 'Оператор 🎥',
+          mail: 'Bazzzber@ikati.space'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TeamsViewer from "@/pages/teams/TeamsViewer.vue";
+import TeamsEditor from "@/pages/teams/TeamsEditor.vue";
 import UsersViewer from "@/pages/users/UsersViewer.vue";
 
 const routes = [
+  { path: "/team/edit", name: 'TeamsEditor', component: TeamsEditor },
   { path: "/team/:id", name: 'TeamsViewer', component: TeamsViewer },
   { path: "/user/:id", name: 'UsersViewer', component: UsersViewer }
 ]
